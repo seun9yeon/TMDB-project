3cdc6a4a366086b47064b80557fcdf2c95888e4c
정승연 <seun9yeon@naver.com>
2025-03-04 15:29:14 +0900

    axios get이 map으로 안불러와지는 문제 있음

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,3 +1,3 @@
 export async function getMovies() {
   const baseURL = ""
-}
+}–
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import { useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function MovieList() {
-  const [posts, setPosts] =useState([]);
+  const [posts, setPosts] = useState([]);
 
-  useEffect{() => {
-    const function fetchPost() {
-    const url = "";
-    const response = await axios.get(url);
-    const data = response.data;
-    setPosts(data);
+  useEffect(() => {
+    async function fetchPost() {
+      const url = "https://api.themoviedb.org/3/movie/now_playing";
+      const response = await axios.get(url);
+      const data = response.data;
+      setPosts(data);
     }
 
     fetchPost();
-  },[]};
-  
+  }, []);
+
   return (
-    <div></div>
-  )
+    <div>
+      <h2>MovieList</h2>
+      <ul>
+        {posts.map((data) => (
+        <h3>{data.title}</h3>
+    ))}
+      </ul>
+    </div>
+  );
 }
