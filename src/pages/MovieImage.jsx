9aa8e609c22bed5d461de73ec2a6ac78ff4eb0e9
seungyeon <seun9yeon@naver.com>
2025-03-05 10:44:26 +0900

    1044

--- src/pages/MovieImage.jsx
+++ src/pages/MovieImage.jsx
@@ -4,9 +4,8 @@ const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500/";
 
 export default function MovieImage({ title, poster_path, vote_average }) {
   return (
-    <div className ='movie-container'>
-      <imge></>
-    </div>
+    // <div className ='movie-container'>
+    // </div>
     <div>MovieImage</div>
   )
 }
